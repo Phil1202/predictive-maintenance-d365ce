ad3468b122d03990363aa97323d5fae7d2763841
Phil1202 <phil.basjan@gmail.com>
2025-06-30 15:29:01 +0200

    changed location of main.py

--- startup.sh
+++ startup.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 echo "Starting FastAPI app with gunicorn..."
-
-# In das Verzeichnis wechseln, das die main.py enthält
-cd api
-
-# Gunicorn starten mit Pfad zu main:app
 exec gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:8000
